94c780997982c4cecd8aa3aed7e1037c9543a911
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-07-16 17:56:40 +0200

    Update runtime names

--- scripts/server.sh
+++ scripts/server.sh
@@ -13,10 +13,10 @@ DEFAULT_LANG="en"
 ALLOWED_MODELS="base" # use comma-separated list of models to allow multiple models, e.g. "base,small,medium"
 ALLOWED_LANGUAGES="en" # use comma-separated list of languages to allow multiple languages, e.g. "auto,en,es,de"
 
-echo "Launching proxy..."
+echo "Launching adapter..."
 echo "Using backend-host=$BACKEND_HOST, backend-port=$BACKEND_PORT, host=$HOST, port=$PORT, default-model=$DEFAULT_MODEL, default-lang=$DEFAULT_LANG, allowed-models=$ALLOWED_MODELS, allowed-languages=$ALLOWED_LANGUAGES"
 
-$SNAP/bin/proxy serve \
+$SNAP/bin/whisperlive-adapter serve \
     --backend-host "$BACKEND_HOST" \
     --backend-port "$BACKEND_PORT" \
     --host "$HOST" \
@@ -26,4 +26,4 @@ $SNAP/bin/proxy serve \
     --allowed-models "$ALLOWED_MODELS" \
     --allowed-languages "$ALLOWED_LANGUAGES"
 
-echo "Proxy terminated."
+echo "Adapter terminated."
