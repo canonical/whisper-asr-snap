c21aa80df71158bc297ea77fc1fd539c424952e6
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-07-22 17:15:10 +0200

    WIP: listen on a unix domain socket

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,23 +4,20 @@ set -e
 
 BACKEND_HOST=$(modelctl get engine-host)
 BACKEND_PORT=$(modelctl get engine-port)
-HOST=$(modelctl get server-host)
-PORT=$(modelctl get server-port)
+SERVER_UNIX_SOCKET=$(modelctl get server-unix-socket)
 
 # TODO: get these from config, and allow multiple models and languages
 DEFAULT_MODEL="base"
 DEFAULT_LANG="en"
 ALLOWED_MODELS="base" # use comma-separated list of models to allow multiple models, e.g. "base,small,medium"
 ALLOWED_LANGUAGES="en" # use comma-separated list of languages to allow multiple languages, e.g. "auto,en,es,de"
-
 echo "Launching adapter..."
 
 set -x
 $SNAP/bin/whisperlive-adapter serve \
     --backend-host "$BACKEND_HOST" \
     --backend-port "$BACKEND_PORT" \
-    --host "$HOST" \
-    --port "$PORT" \
+    --unix-socket "$SERVER_UNIX_SOCKET" \
     --model "$DEFAULT_MODEL" \
     --language "$DEFAULT_LANG" \
     --allowed-models "$ALLOWED_MODELS" \
