d7f8ec0bc8a961f8857f1e8c17eeda3b3e899982
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-07-24 17:12:59 +0200

    refactor openai-adapter

--- engines/cpu/openai-adapter.sh
+++ engines/cpu/openai-adapter.sh
@@ -10,27 +10,32 @@ ADAPTER_PORT=$(modelctl get openai.port)
 DEFAULT_LANG="en"
 ALLOWED_LANGUAGES="en" # use comma-separated list of languages to allow multiple languages, e.g. "auto,en,es,de"
 
+get_installed_models_aliases() {
+    local aliases=()
+    while IFS= read -r line; do
+        # Split the jq output into the component name and description
+        local first_component_name="${line%%|*}"
+        local alias="${line#*|}"
+
+        # Check if the component is installed by checking if its directory exists
+        if [[ -d "$SNAP_COMPONENTS/$first_component_name" ]]; then
+            aliases+=("$alias")
+        fi
+    done < <(
+        # Output format per item: "first-component-name|alias"
+        modelctl list-models --format=json | jq -r '.models[] | "\(.components[0])|\(.alias)"'
+    )
+    # Return a comma-separated list of aliases
+    (IFS=,; echo "${aliases[*]}") 
+}
+
 echo "Launching adapter..."
 
 # Get the alias of the active model
 active_model_alias=$(modelctl list-models --format=json | jq -r '. as $root | .models[] | select(.name == $root."active-model") | .alias')
 
 # Get installed models
-installed_model_aliases=()
-while IFS= read -r line; do
-    # Split the jq output into the component name and description
-    first_component_name="${line%%|*}"
-    alias="${line#*|}"
-
-    # Check if the component directory actually exists
-    if [[ -d "$SNAP_COMPONENTS/$first_component_name" ]]; then
-        installed_model_aliases+=("$alias")
-    fi
-done < <(
-    # Output format per item: "first-component-name|alias"
-    modelctl list-models --format=json | jq -r '.models[] | "\(.components[0])|\(.alias)"'
-)
-installed_model_aliases_str=$(IFS=,; echo "${installed_model_aliases[*]}")
+installed_model_aliases_str=$(get_installed_models_aliases)
 
 set -x
 $SNAP/bin/whisperlive-adapter serve \
