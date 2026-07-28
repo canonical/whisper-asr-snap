51959839f8004698d641ed05598cac0ea1563f96
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-07-28 17:35:07 +0200

    Remove model hot-swapping support
    Model hot-swapping allows for a client to change the session model to another installed model without sudo access and without restarting the snap, but for now it has issues with model configuration (i.e. allowed languages)

--- engines/cpu/openai-adapter.sh
+++ engines/cpu/openai-adapter.sh
@@ -9,32 +9,10 @@ ADAPTER_PORT=$(modelctl get http.port)
 
 DEFAULT_LANG=$(modelctl get transcription-language)
 
-get_installed_models_aliases() {
-    local aliases=()
-    while IFS= read -r line; do
-        # Split the jq output into the component name and description
-        local first_component_name="${line%%|*}"
-        local alias="${line#*|}"
-
-        # Check if the component is installed by checking if its directory exists
-        if [[ -d "$SNAP_COMPONENTS/$first_component_name" ]]; then
-            aliases+=("$alias")
-        fi
-    done < <(
-        # Output format per item: "first-component-name|alias"
-        modelctl list-models --format=json | jq -r '.models[] | "\(.components[0])|\(.alias)"'
-    )
-    # Return a comma-separated list of aliases
-    (IFS=,; echo "${aliases[*]}") 
-}
-
 echo "Launching adapter..."
 
 # Get the alias of the active model
 active_model_alias=$(modelctl list-models --format=json | jq -r '. as $root | .models[] | select(.name == $root."active-model") | .alias')
-
-# Get installed models
-installed_model_aliases_str=$(get_installed_models_aliases)
 
 set -x
 $SNAP/bin/whisperlive-adapter serve \
