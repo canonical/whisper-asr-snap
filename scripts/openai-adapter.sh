91482a7dc577461ce18b5db2da815d9322df8b14
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-09-16 15:42:54 +0200

    Write provider.env in the shared content interface

--- scripts/openai-adapter.sh
+++ scripts/openai-adapter.sh
@@ -2,12 +2,37 @@
 
 set -euo pipefail
 
-# TODO: introduce the export-shared-configs script
-# Export the configuration for content sharing
-# This must be done each time the server is started to expose the actual configuration
-# $SNAP/bin/export-shared-configs.sh
 
 engine="$(modelctl status --format=json | jq -r .engine)"
+
+share_provider() {
+    local status_json=$(modelctl status --format=json)
+    local provider_env_content="SNAP_NAME=$SNAP_NAME\nSNAP_INSTANCE_NAME=$SNAP_INSTANCE_NAME\n"
+
+    # if status_json.entrypoints has a "openai" entry
+    if [ "$(echo "$status_json" | jq -e '.entrypoints | has("openai")')" = "true" ]; then
+        local openai_base_url=$(echo "$status_json" | jq -r '.entrypoints.openai.url')
+        provider_env_content+="OPENAI_BASE_URL=$openai_base_url\n"
+    fi
+
+    # if status_json.entrypoints has a "openai-unix" entry
+    if [ "$(echo "$status_json" | jq -e '.entrypoints | has("openai-unix")')" = "true" ]; then
+        local full_socket_path=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket"')
+        local socket_filename=$(basename "$full_socket_path")
+        local socket_url=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket-url"')
+        provider_env_content+="OPENAI_UNIX_BASE_URL=$socket_filename\n"
+        provider_env_content+="OPENAI_UNIX_SOCKET_URL=$socket_url\n"
+    fi
+
+    local share_dir="$SNAP_COMMON/share/provider"
+    if [ ! -d "$share_dir" ]; then
+        echo "Share directory does not exist: $share_dir"
+        exit 1
+    fi
+    
+    local env_file_path="$share_dir/provider.env"
+    echo -e "$provider_env_content" > "$env_file_path"
+}
 
 ensure_unix_socket_in_shared_content() {
     local share_dir="$SNAP_COMMON/share/provider"
@@ -19,5 +44,7 @@ ensure_unix_socket_in_shared_content() {
 }
 
 ensure_unix_socket_in_shared_content
+share_provider
 
+# TODO: use modelctl run --share-provider instead of share_provider() once every feature is implemented
 exec modelctl run -- "$SNAP/engines/$engine/openai-adapter.sh" "$@"
