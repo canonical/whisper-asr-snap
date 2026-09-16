6c6825c558d58db05fcd9426ec9caafed49fa5a1
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-09-16 17:48:09 +0200

    fix: split declaration from assignment in local bash variable to not mask exit codes

--- scripts/openai-adapter.sh
+++ scripts/openai-adapter.sh
@@ -6,20 +6,25 @@ set -euo pipefail
 engine="$(modelctl status --format=json | jq -r .engine)"
 
 share_provider() {
-    local status_json=$(modelctl status --format=json)
+    local status_json
+    status_json=$(modelctl status --format=json)
     local provider_env_content="SNAP_NAME=$SNAP_NAME\nSNAP_INSTANCE_NAME=$SNAP_INSTANCE_NAME\n"
 
     # if status_json.entrypoints has a "openai" entry
     if [ "$(echo "$status_json" | jq -e '.entrypoints | has("openai")')" = "true" ]; then
-        local openai_base_url=$(echo "$status_json" | jq -r '.entrypoints.openai.url')
+        local openai_base_url
+        openai_base_url=$(echo "$status_json" | jq -r '.entrypoints.openai.url')
         provider_env_content+="OPENAI_BASE_URL=$openai_base_url\n"
     fi
 
     # if status_json.entrypoints has a "openai-unix" entry
     if [ "$(echo "$status_json" | jq -e '.entrypoints | has("openai-unix")')" = "true" ]; then
-        local full_socket_path=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket"')
-        local socket_filename=$(basename "$full_socket_path")
-        local socket_url=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket-url"')
+        local full_socket_path
+        local socket_filename
+        local socket_url
+        full_socket_path=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket"')
+        socket_filename=$(basename "$full_socket_path")
+        socket_url=$(echo "$status_json" | jq -r '.entrypoints."openai-unix"."unix-socket-url"')
         provider_env_content+="OPENAI_UNIX_BASE_URL=$socket_filename\n"
         provider_env_content+="OPENAI_UNIX_SOCKET_URL=$socket_url\n"
     fi
@@ -36,11 +41,14 @@ share_provider() {
 
 ensure_unix_socket_in_shared_content() {
     local share_dir="$SNAP_COMMON/share/provider"
-    local unix_socket_path=$(modelctl get http.unix-socket)
+    local unix_socket_path
+    unix_socket_path=$(modelctl get http.unix-socket)
     if [ -n "$unix_socket_path" ]; then
         # Paths are normalized to prevent path traversal (i.e., use of "../" in the path)
-        local normalized_share_dir=$(realpath -m -- "$share_dir")
-        local normalized_socket_path=$(realpath -m -- "$unix_socket_path")
+        local normalized_share_dir
+        local normalized_socket_path
+        normalized_share_dir=$(realpath -m -- "$share_dir")
+        normalized_socket_path=$(realpath -m -- "$unix_socket_path")
         if [[ "$normalized_socket_path" != "$normalized_share_dir/"* ]]; then
             echo "Unix socket path ($unix_socket_path) is not in the expected share directory ($share_dir)"
             exit 1
