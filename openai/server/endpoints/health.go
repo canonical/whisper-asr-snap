44f8a1eeb20b43d05a3fc871da333408ddc7f989
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-09-17 11:04:50 +0200

    Use a json object as a response from the health endpoint

--- openai/server/endpoints/health.go
+++ openai/server/endpoints/health.go
@@ -6,6 +6,6 @@ import "net/http"
 func Health() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("ok"))
+		_, _ = w.Write([]byte("{\"status\":\"ok\"}\n"))
 	}
 }
